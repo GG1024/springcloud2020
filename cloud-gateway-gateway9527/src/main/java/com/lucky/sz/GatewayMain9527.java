7cb41a0b67bdfa03d04e6c53febaa72bd5f27c48
oyxg1024_pro <oyxg1024@163.com>
2021-02-22 16:00:42 +0800

    gateway入门配置 测试失败

---
+++ cloud-gateway-gateway9527/src/main/java/com/lucky/sz/GatewayMain9527.java
@@ -0,0 +1,25 @@
+package com.lucky.sz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+import java.io.Serializable;
+
+/**
+ * @FileName: GatewayMain9527.java
+ * @description:
+ * @author: 欧阳小广
+ * @Date: 2021-02-22
+ **/
+@SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
+public class GatewayMain9527 implements Serializable {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayMain9527.class, args);
+    }
+
+}
