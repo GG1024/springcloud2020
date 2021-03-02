b79c53c7bf3018ae1f419238f18d1b5a55867d98
oyxg1024_pro <oyxg1024@163.com>
2021-03-02 11:52:45 +0800

    令牌桶算法实现乐观锁+限流

--- miaosha/src/main/java/com/lucky/sz/controller/StockController.java
+++ miaosha/src/main/java/com/lucky/sz/controller/StockController.java
@@ -1,11 +1,13 @@
 package com.lucky.sz.controller;
 
+import com.google.common.util.concurrent.RateLimiter;
 import com.lucky.sz.service.OrderService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @description:
@@ -22,7 +24,7 @@ public class StockController {
 
 
     //创建令牌桶实例
-    //private RateLimiter rateLimiter = RateLimiter.create(40);
+    private RateLimiter rateLimiter = RateLimiter.create(40);
 
 
     //秒杀方法
@@ -30,10 +32,10 @@ public class StockController {
     public String sale(Integer id) {
         //System.out.println("秒杀商品的id = " + id);
         //加入令牌桶的限流措施
-//        if (!rateLimiter.tryAcquire(3, TimeUnit.SECONDS)) {
-//            log.info("抛弃请求: 抢购失败,当前秒杀活动过于火爆,请重试");
-//            return "抢购失败,当前秒杀活动过于火爆,请重试!";
-//        }
+        if (!rateLimiter.tryAcquire(3, TimeUnit.SECONDS)) {
+            log.info("抛弃请求: 抢购失败,当前秒杀活动过于火爆,请重试");
+            return "抢购失败,当前秒杀活动过于火爆,请重试!";
+        }
         try {
             //根据商品id创建订单,返回创建订单的id
             int orderId = orderService.createOrder(id);
