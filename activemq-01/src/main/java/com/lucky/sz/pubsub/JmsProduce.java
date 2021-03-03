88051dcaad027cf713ec099b41a291451649575c
oyxg1024_pro <oyxg1024@163.com>
2021-03-03 17:10:09 +0800

    消息头

--- activemq-01/src/main/java/com/lucky/sz/pubsub/JmsProduce.java
+++ activemq-01/src/main/java/com/lucky/sz/pubsub/JmsProduce.java
@@ -16,7 +16,7 @@ public class JmsProduce implements Serializable {
 
 
     private static final String ACTIVEMQ_URL = "tcp://192.168.92.129:61616";
-    private static final String TOPIC_NAME  = "topic01";
+    private static final String TOPIC_NAME = "topic01";
 
     public static void main(String[] args) throws JMSException {
         //1.创建连接工厂，按照给定的URL，采用默认的用户名密码
@@ -35,6 +35,35 @@ public class JmsProduce implements Serializable {
         for (int i = 0; i < 6; i++) {
             //7.通过session创建消息
             TextMessage textMessage = session.createTextMessage("TOPIC_NAME---" + i);
+            //这里指定传递的消息目的地
+            textMessage.setJMSDestination(topic);
+            /*
+             * 持久模式和非持久模式
+             * 1：DeliveryMode.NON_PERSISTENT 非持久
+             * 2：DeliveryMode.PERSISTENT 持久
+             * 一条持久性的消息，应该被传送“一次仅仅一次”意味着如果jms提供者出现故障，该消息并不会丢失，他会在服务器恢复之后再次传递
+             * 一条非持久的消息，最多会传递一次，意味着服务器出现故障，该消息将会永远丢失。
+             */
+            textMessage.setJMSDeliveryMode(DeliveryMode.PERSISTENT);
+
+            /*
+             *设置消息的过期时间，默认永不过期
+             * 消息过期时间，等于Destination的send方法中timeToLive值加上发送时刻GMT时间值
+             * 如果timeToLive等于0，则JMSException被设为0，表示该消息用不过期
+             * 如果发送后，在过期时间后还没有发送到目的地，则该消息被清除。
+             */
+            textMessage.setJMSExpiration(1000);
+            /*
+             * 消息优先级，0-9十个级别，0-4普通消息，5-9加急消息
+             * JMS规范不严格要求mq按照这十个级别去发送消息，但必须保证加急消息优先于普通消息，默认级别是4
+             */
+            textMessage.setJMSPriority(10);
+            /*
+             *唯一标识每个消息的标识，MQ默认生成，也可自己指定
+             */
+            textMessage.setJMSMessageID("ABCD");
+
+
             //8.使用指定好目的地的消息生产者发送消息
             messageProducer.send(textMessage);
         }
@@ -46,8 +75,6 @@ public class JmsProduce implements Serializable {
 
 
     }
-
-
 
 
 }
