01c1b5544b0d3a4390618e27e3fc3e4c53087015
oyxg1024_pro <oyxg1024@163.com>
2021-03-04 15:17:05 +0800

    springboot 整合activemqTopic,非持久

---
+++ boot-mq-topic-producer/src/main/java/com/lucky/sz/TopicMain.java
@@ -0,0 +1,22 @@
+package com.lucky.sz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.io.Serializable;
+
+/**
+ * @FileName: TopicMain.java
+ * @description:
+ * @author: 欧阳小广
+ * @Date: 2021-03-04
+ **/
+@SpringBootApplication
+public class TopicMain implements Serializable {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TopicMain.class, args);
+    }
+
+}
